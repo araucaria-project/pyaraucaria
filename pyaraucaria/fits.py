62e1ecd7c224f833dbccb6f52923918e576a0a4a
MMiirrkk <cot@wp.pl>
2023-04-10 14:41:35 -0400

    more opt

--- pyaraucaria/fits.py
+++ pyaraucaria/fits.py
@@ -3,7 +3,16 @@ from collections import OrderedDict
 from astropy.io import fits
 import numpy as np
 
-def save_fits_from_array(array, folder: str, file_name: str, header, overwrite: bool = False, dtyp: str = 'int32'):
+def save_fits_from_array(array,
+                         folder: str,
+                         file_name: str,
+                         header,
+                         overwrite: bool = False,
+                         dtyp: str = 'int32',
+                         do_not_scale_image_data: bool = False,
+                         ignore_blank: bool = False,
+                         uint: bool = False,
+                         scale_back: bool or None = None):
     """
     Save fits file from array to selected location.
     Parameters
@@ -13,7 +22,11 @@ def save_fits_from_array(array, folder: str, file_name: str, header, overwrite: 
     file_name - file name without '.fits', example: 'file_no_2233'
     header - fits header in dict format, example: {"FITS_STD": "beta_1", "TEL": "iris"}
     overwrite - overwrite existing file, default=False
-    dtyp - array type [str], exammple: 'int16', 'int32'
+    dtyp - array type [str], example: 'int16', 'int32'
+    do_not_scale_image_data - astropy PrimaryHDU  parameter
+    ignore_blank - astropy PrimaryHDU  parameter
+    uint - astropy PrimaryHDU  parameter
+    scale_back - astropy PrimaryHDU  parameter
     """
     if os.path.splitext(file_name)[1] == "":
         file_name = f"{file_name}.fits"
@@ -23,33 +36,47 @@ def save_fits_from_array(array, folder: str, file_name: str, header, overwrite: 
 
     if isinstance(header, dict):
         for n in header.keys():
-            hdr[n] = header[n][0]
-            hdr.comments[n] = header[n][1]
+            try:
+                hdr[n] = header[n][0]
+                hdr.comments[n] = header[n][1]
+            except (LookupError, TypeError):
+                hdr[n] = header[n]
+    elif isinstance(header, fits.Header):
+        hdr = header
     else:
         hdr["OCASTD"] = "No fits header provided"
-        
-    dtyp = np.int32
+
     if dtyp=='int32':
         narray = np.array(array, dtype=np.int32)
     elif dtyp=='int16':
         narray = np.array(array, dtype=np.int16)
     elif dtyp=='sideint16':
-        s_array = array - 32768
+        s_array = np.array(array) - 32768
         narray = np.array(s_array, dtype=np.int16)
     elif dtyp=='none':
         narray = array
-    hdu = fits.PrimaryHDU(data=narray, header=hdr)
+    hdu = fits.PrimaryHDU(data=narray,
+                          header=hdr,
+                          do_not_scale_image_data=do_not_scale_image_data,
+                          ignore_blank=ignore_blank,
+                          uint=uint,
+                          scale_back=scale_back)
     hdul = fits.HDUList([hdu])
     hdul.writeto(file_name, overwrite=overwrite)
 
-def fits_header(bzero=32768,
-                oca_std="BETA2",
+# Lets Follow the FitS standard version 4, as defined in
+# https://fits.gsfc.nasa.gov/fits_standard.html
+# https://fits.gsfc.nasa.gov/standard40/fits_standard40aa-le.pdf
+# https://heasarc.gsfc.nasa.gov/docs/fcg/common_dict.html may be also useful
+
+def fits_header(oca_std="BETA2",
                 obs="OCA",
                 obs_lat='',
                 parsed_obs_lat='',
                 obs_lon='',
                 parsed_obs_lon='',
                 obs_elev='',
+                origin='CAMK PAN',
                 tel_id='',
                 utc_now='',
                 jd='',
@@ -82,12 +109,12 @@ def fits_header(bzero=32768,
                 ):
 
     _header = OrderedDict({
-        "BZERO": bzero,
         "OCASTD": (oca_std, "OCA FITS HDU standard version"),
-        "OBS": (obs, "Cerro Armazones Observatory"),
-        "OBS-LAT": (parsed_obs_lat, f"[deg] Observatory east longitude {obs_lat}"),
+        "OBSERVAT": (obs, "Cerro Armazones Observatory"),
+        "OBS-LAT": (parsed_obs_lat, f"[deg] Observatory longitude {obs_lat}"),
         "OBS-LONG": (parsed_obs_lon, f"[deg] Observatory latitude {obs_lon}"),
         "OBS-ELEV": (obs_elev, f"[m] Observatory elevation"),
+        "ORIGIN": (origin, "Institution created this FITS file"),
         "TEL": (tel_id, ''),
         "DATE-OBS": (utc_now, "DateTime of observation start"),
         "JD": (jd, "Julian date of observation start"),
