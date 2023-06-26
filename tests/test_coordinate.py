b957d8e116b71694abb33e4bc91b4d7b7771e851
MMiirrkk <cot@wp.pl>
2023-06-26 08:42:49 +0200

    str_to_def_out

--- tests/test_coordinate.py
+++ tests/test_coordinate.py
@@ -146,7 +146,7 @@ class TestAzAlt2RaDecAstropy(unittest.TestCase):
                                               calc_time=tim)
         ra_0 = ra_to_decimal(ra)
         dec_0 = dec_to_decimal(dec)
-        self.assertAlmostEqual(ra_0, ra_2, places=3)
+        self.assertAlmostEqual(ra_0, ra_2, places=3)git s
         self.assertAlmostEqual(dec_0, dec_2, places=3)
 
 
