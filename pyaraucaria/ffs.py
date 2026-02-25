838924b2ddac2b50e122633eae9be692604d7d08
astromg <mareksgorski@gmail.com>
2026-02-25 21:14:01 +0100

    laplace kernel added

--- pyaraucaria/ffs.py
+++ pyaraucaria/ffs.py
@@ -810,7 +810,7 @@ class FFS:
         return kernel_l, kernel_r
 
     @staticmethod
-    def laplace_kernel(size, sigma):
+    def laplace_kernel():
         kernel = [[0,-1,0],[-1,4,-1],[0,-1,0]]
         return kernel
 
